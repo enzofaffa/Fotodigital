abb3c9100256fa11002ff0d6126457c8b6039f9b
Enzo Faffa <enzo.faffa@artgroup-spa.com>
2018-07-15 16:58:49 +0200

    added API for list files and folder

--- InnovativeTool/Form1.Designer.cs
+++ InnovativeTool/Form1.Designer.cs
@@ -29,6 +29,9 @@
         private void InitializeComponent()
         {
             this.buttonConnect = new System.Windows.Forms.Button();
+            this.checkedListBoxOrder = new System.Windows.Forms.CheckedListBox();
+            this.buttonProcess = new System.Windows.Forms.Button();
+            this.textBoxLog = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // buttonConnect
@@ -39,22 +42,58 @@
             this.buttonConnect.TabIndex = 0;
             this.buttonConnect.Text = "button1";
             this.buttonConnect.UseVisualStyleBackColor = true;
+            this.buttonConnect.Click += new System.EventHandler(this.buttonConnect_Click);
+            // 
+            // checkedListBoxOrder
+            // 
+            this.checkedListBoxOrder.CheckOnClick = true;
+            this.checkedListBoxOrder.FormattingEnabled = true;
+            this.checkedListBoxOrder.Location = new System.Drawing.Point(34, 133);
+            this.checkedListBoxOrder.Name = "checkedListBoxOrder";
+            this.checkedListBoxOrder.Size = new System.Drawing.Size(178, 184);
+            this.checkedListBoxOrder.TabIndex = 1;
+            this.checkedListBoxOrder.SelectedIndexChanged += new System.EventHandler(this.checkedListBoxOrder_SelectedIndexChanged);
+            // 
+            // buttonProcess
+            // 
+            this.buttonProcess.Location = new System.Drawing.Point(69, 356);
+            this.buttonProcess.Name = "buttonProcess";
+            this.buttonProcess.Size = new System.Drawing.Size(75, 23);
+            this.buttonProcess.TabIndex = 3;
+            this.buttonProcess.Text = "Process";
+            this.buttonProcess.UseVisualStyleBackColor = true;
+            this.buttonProcess.Click += new System.EventHandler(this.buttonProcess_Click);
+            // 
+            // textBoxLog
+            // 
+            this.textBoxLog.Location = new System.Drawing.Point(13, 408);
+            this.textBoxLog.Name = "textBoxLog";
+            this.textBoxLog.ReadOnly = true;
+            this.textBoxLog.Size = new System.Drawing.Size(725, 20);
+            this.textBoxLog.TabIndex = 4;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBoxLog);
+            this.Controls.Add(this.buttonProcess);
+            this.Controls.Add(this.checkedListBoxOrder);
             this.Controls.Add(this.buttonConnect);
             this.Name = "Form1";
             this.Text = "InnovativeTool";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button buttonConnect;
+        private System.Windows.Forms.CheckedListBox checkedListBoxOrder;
+        private System.Windows.Forms.Button buttonProcess;
+        private System.Windows.Forms.TextBox textBoxLog;
     }
 }
 
--- InnovativeTool/Form1.cs
+++ InnovativeTool/Form1.cs
@@ -7,14 +7,171 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using System.Net;
+using Lib.Common;
 
 namespace InnovativeTool
 {
     public partial class Form1 : Form
     {
+        #region Costanti
+        private const string NEW_ORDER_DIRNAME = "Orders";
+        private const string PROCESSED_ORDER_DIRNAME = "Scaricati";
+
+        private const string TEST_FOLDER = "../../_test_";
+        #endregion
+
+        #region Proprietà
+        #endregion
+
+        #region Delegati
+        #endregion
+
+        #region Membri privati
+        static private int DirLevel = 0;
+        #endregion
+
+        #region Membri statici
+        #endregion
+
+        #region Costruttori
         public Form1()
         {
             InitializeComponent();
+
+            string path = Directory.GetCurrentDirectory();
+
+            /* fill order list */
+            List<string> orderList;
+            ListDir(Path.Combine(TEST_FOLDER, NEW_ORDER_DIRNAME),out orderList);
+            foreach(string s in orderList)
+            {
+                string dirName = Path.GetFileName(s);
+                checkedListBoxOrder.Items.Add(dirName);
+            }
         }
+        #endregion
+
+        #region Metodi privati
+        private void ListDir(string searchDirectory, out List<string> dirList)
+        {
+            dirList = new List<string>();
+
+            try
+            {
+                foreach (string d in Directory.GetDirectories(searchDirectory))
+                {
+                    dirList.Add(d);
+                }
+            }
+            catch (System.Exception excpt)
+            {
+                textBoxLog.Text = excpt.Message;
+            }
+                   
+            return;
+        }
+#if true
+        private void ListFiles(string searchDirectory, out List<string> fileList)
+        {
+            fileList = new List<string>();
+
+            try
+            {
+                foreach (string filename in Directory.GetFiles(searchDirectory))
+                {
+                    fileList.Add(filename);
+                }
+            }
+            catch (System.Exception excpt)
+            {
+                textBoxLog.Text = excpt.Message;
+            }
+
+            return;
+        }
+#endif
+
+#if false
+        private void DirSearch(string sDir)
+        {
+#if true
+            try
+            {
+                foreach (string d in Directory.GetDirectories(sDir))
+                {                   
+                    if (d.Contains(NEW_ORDER_DIRNAME) == false)
+                    {
+                        /* nothing to do */
+                    }
+                    else
+                    {
+                        string s1 = Path.GetFileName(d);
+
+
+                        foreach (string f in Directory.GetFiles(d))
+                        {
+#if false
+                            Console.WriteLine(f);
+
+                            //string s2 = Path.GetFileName(d);
+                            //checkedListBox1.Items.Add(s2);
+
+                            //listBox1.Items.Add(s2);
+                            //listBox1.Items.Add(DirLevel);
+                            // checkedl
+#endif
+                        }
+                    }
+                }            
+            }
+            catch (System.Exception excpt)
+            {
+                textBoxLog.Text = excpt.Message;
+            }
+#else
+                        foreach (string file in Directory.EnumerateFiles(sDir, "*.*", SearchOption.AllDirectories))
+            {
+                Console.WriteLine(file);
+            }
+#endif
+        }
+#endif
+        #endregion
+
+        #region Metodi pubblici
+        #endregion
+
+        #region Eventi
+        private void buttonConnect_Click(object sender, EventArgs e)
+        {
+#if true
+            FtpClient ftpClient = new FtpClient("ftp://speedtest.tele2.net", "anonymous", "");
+            string[] dirList = ftpClient.directoryListDetailed("");
+#elif false
+#endif
+        }
+        private void buttonProcess_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void checkedListBoxOrder_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string item = checkedListBoxOrder.SelectedItem.ToString();
+
+            List<string> fileList;
+            ListFiles(Path.Combine(TEST_FOLDER, NEW_ORDER_DIRNAME, item),out fileList);
+        }
+#endregion
+
+#region Membri Background worker
+#endregion
+
+#region Timers
+#endregion
+
+#region Callbacks
+#endregion
     }
 }
